5369408178e78897bd3a623275470224618479ad
Orix <orix.auyeung@hunt.social>
2020-02-20 17:27:31 +0800

    add analyse.py to extract+analyse keywords

---
+++ analyse.py
@@ -0,0 +1,33 @@
+import os
+import json
+from collections import Counter
+
+SUBTITLE_PATH = "./subtitle"
+
+
+def load_subtitles():
+    subtitles = []
+    subtitle_files = [os.path.join(SUBTITLE_PATH, x) for x in os.listdir(SUBTITLE_PATH)]
+    for subtitle_file in subtitle_files:
+        with open(subtitle_file, 'r') as f:
+            subtitle = json.loads(f.read())
+            subtitles.append(subtitle)
+    return subtitles
+
+
+def get_keywords_by_programme(entities, programme):
+    programme_subtitles = filter(lambda x: x['programme'] == programme, subtitles)
+    for subtitle in programme_subtitles:
+        entities += subtitle['entities']
+    filtered_entities = filter(lambda x: x['type'] != 'NUMBER' and x['name'] != "人", entities)
+    words = [entity['name'] for entity in filtered_entities]
+    return words
+
+
+if __name__ == "__main__":
+    entities = []
+    subtitles = load_subtitles()
+    programme = "THKCCT2020M05301836"
+    words = get_keywords_by_programme(entities, programme)
+    print(words)
+    print(dict(Counter(words)))
